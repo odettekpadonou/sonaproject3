d7d3e379702d4244a934ef48bd8733163bdd9761
“odettekpadonou” <afiavikpadonou@gmail.com>
2022-02-26 12:33:32 -0500

    new change

--- sona.sh
+++ sona.sh
@@ -12,41 +12,34 @@
             
       echo: Step 1: Java 11 installation
 
-            sudo yum update -y
+      sudo yum update -y
+      sudo yum install java-11-openjdk-devel -y
+      sudo yum install java-11-openjdk -y
 
-	          sudo yum install java-11-openjdk-devel -y
+     echo: Step 2: Download SonarQube latest versions on your server
 
-		        sudo yum install java-11-openjdk -y
+     cd /opt 
 
-			echo: Step 2: Download SonarQube latest versions on your server
+     sudo yum install wget -y
 
-			         cd /opt 
+      sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
 
-				 	  sudo yum install wget -y
+       echo: Step 3: Extract packages
 
-					  	  sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.3.0.51899.zip
+       sudo yum install unzip -y
+       sudo unzip /opt/sonarqube-9.3.0.51899.zip
 
-						  echo: Step 3: Extract packages
+     echo Step 4: Change ownership to the user and Switch to Linux binaries directory to start service
 
-						          sudo yum install unzip -y
+      sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
 
-							  	 sudo unzip /opt/sonarqube-9.3.0.51899.zip
+      cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
 
-								 echo Step 4: Change ownership to the user and Switch to Linux binaries directory to start service
+       ./sonar.sh start
 
-								         sudo chown -R vagrant:vagrant /opt/sonarqube-9.3.0.51899
+         echo:connect to Sonarqube
+        http://<your-ip-address>:9000
+        sudo firewall-cmd --permanent --add-port=9000/tcp
+       sudo firewall-cmd --reload
 
-									 	cd /opt/sonarqube-9.3.0.51899/bin/linux-x86-64
-
-											./sonar.sh start
-
-											echo:connect to Sonarqube
-
-											    http://<your-ip-address>:9000
-
-											        sudo firewall-cmd --permanent --add-port=9000/tcp
-
-												    sudo firewall-cmd --reload
-
-
-												        echo: installation  successfuly.
+        echo: installation  successfuly.
